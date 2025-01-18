8472bf225edd4b04cadfae6df0715f28e084df0d
ltbreno <132773962+ltbreno@users.noreply.github.com>
2025-01-18 12:42:31 -0300

    Add LoginBottomSheetViewModel

---
+++ Reminder/Reminder/Sources/Features/LoginBottomSheet/ViewModel/LoginBottomSheetViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  LoginBottomSheetViewModel.swift
+//  Reminder
+//
+//  Created by Breno Lopes on 18/01/25.
+//
+
+import Foundation
+
+class LoginBottomSheetViewModel {
+    func doAuth(usernameLogin: String, password: String) {
+        print(usernameLogin) // Local para chamada do backend ( Usecase, Service )
+    }
+}
