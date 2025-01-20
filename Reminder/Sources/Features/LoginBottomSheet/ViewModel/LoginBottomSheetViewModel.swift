d7491dfe60028069b842a8b66ea240b076132e25
Breno Lopes <brenosas2003@gmail.com>
2025-01-20 13:16:16 -0300

    handling login error

--- Reminder/Sources/Features/LoginBottomSheet/ViewModel/LoginBottomSheetViewModel.swift
+++ Reminder/Sources/Features/LoginBottomSheet/ViewModel/LoginBottomSheetViewModel.swift
@@ -10,12 +10,13 @@ import Firebase
 
 class LoginBottomSheetViewModel {
     var successResult: ((String) -> Void )?
+    var errorResult: ((String) -> Void )?
     
     func doAuth(usernameLogin: String, password: String) {
         print(usernameLogin)
         Auth.auth().signIn(withEmail: usernameLogin, password: password) { [weak self] authResult, error in
             if let error = error {
-                print("Autenticação não teve sucesso \(error)")
+                self?.errorResult?("erro ao realizar login, verifique as credenciais")
             } else {
                 self?.successResult?(usernameLogin)
             }
