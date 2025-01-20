e7feee8f7e80ef9176f04568c01b7a323865b625
Breno Lopes <brenosas2003@gmail.com>
2025-01-20 10:55:05 -0300

    Create UserDefaults

--- Reminder/Sources/Features/DependencyInjection/ViewControllersFactory/ViewControllersFactory.swift
+++ Reminder/Sources/Features/DependencyInjection/ViewControllersFactory/ViewControllersFactory.swift
@@ -9,14 +9,13 @@ import Foundation
 import UIKit
 
 final class ViewControllersFactory: ViewControllersFactoryProtocol {
-    func makeSplashViewController(flowDelegate: SplashFlowDelegate) ->
-    SplashViewController {
-        let viewController = SplashViewController(flowDelegate: flowDelegate)
+    func makeSplashViewController(flowDelegate: SplashFlowDelegate) -> SplashViewController {
+        let contentView = SplashView()
+        let viewController = SplashViewController(contentView: contentView , flowDelegate: flowDelegate)
         return viewController
     }
     
-    func makeLoginBottomSheetViewController(flowDelegate: LoginBottomSheetFlowDelegate) ->
-    LoginBottomSheetViewController {
+    func makeLoginBottomSheetViewController(flowDelegate: LoginBottomSheetFlowDelegate) -> LoginBottomSheetViewController {
         let contentView = LoginBottomSheetView()
         let viewController = LoginBottomSheetViewController(contentView: contentView, flowDelegate: flowDelegate)
         return viewController
---
+++ Reminder/Sources/Features/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Reminder
+//
+//  Created by Breno Lopes on 20/01/25.
+//
+
+import Foundation
+
+struct User: Codable {
+    let email: String
+    let isUserSaved: Bool
+}
+
